6cbed4e657baaddee71846e12762f0622a19bbdf
shreytec11 <71381075+shreytec11@users.noreply.github.com>
2022-10-11 11:15:30 +0530

    Time: 1185 ms (9.14%), Space: 13.9 MB (47.52%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,29 +1,41 @@
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        
+#         n = [0]*len(nums)
+        
+#         for i in range(len(nums)):
+            
+#             j = i+1
+            
+#             while(j<len(nums)):
+                
+#                 if nums[i] > nums[j]:
+                    
+#                     n[i] += 1
+                    
+#                     j += 1
+                
+#                 elif nums[i] == nums[j]:
+                    
+#                     j += 1
+                    
+#                 else:
+#                     n[j] += 1
+                    
+#                     j += 1
+#         return(n)
+                    
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        
-        n = [0]*len(nums)
-        
-        for i in range(len(nums)):
-            
-            j = i+1
-            
-            while(j<len(nums)):
-                
-                if nums[i] > nums[j]:
+        l=len(nums)
+        count=0
+        out=[]
+        for i in range(l):
+            count=0
+            for j in range(l):
+                if i!=j and nums[j]<nums[i]:
+                    count+=1
+            out.append(count)
                     
-                    n[i] += 1
-                    
-                    j += 1
-                
-                elif nums[i] == nums[j]:
-                    
-                    j += 1
-                    
-                else:
-                    n[j] += 1
-                    
-                    j += 1
-        return(n)
-                    
-            
+        return out
         
