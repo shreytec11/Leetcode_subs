41ef409b641588b2c764fa686bf92de21937fbe6
shreytec11 <71381075+shreytec11@users.noreply.github.com>
2022-10-13 00:59:54 +0530

    Time: 171 ms (78.24%), Space: 14.4 MB (89.20%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,19 +1,24 @@
+# class Solution:
+#     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        
+#         d = {}
+        
+#         for i in range(len(names)):
+            
+#             d[heights[i]] = names[i]
+        
+#         for i in range(len(heights)):
+            
+#             m = max(heights)
+#             names[i] = d[m]
+#             heights.remove(m)
+            
+#         return(names)
+        
 class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
-        d = {}
-        
-        for i in range(len(names)):
-            
-            d[heights[i]] = names[i]
-        
-        for i in range(len(heights)):
-            
-            m = max(heights)
-            names[i] = d[m]
-            heights.remove(m)
-            
-        return(names)
-        
-        
-        
+    def sortPeople(self, n: List[str], h: List[int]) -> List[str]:
+        a = [] 
+        for i in range(len(n)):
+            a.append((h[i],n[i])) 
+        a.sort(reverse = True) 
+        return [nn for hh,nn in a] 
