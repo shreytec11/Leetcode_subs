0b2c49a3ae17ac409246bd42af9606f4ff80737a
shreytec11 <71381075+shreytec11@users.noreply.github.com>
2023-02-07 15:38:37 +0530

    Added solution - LeetHub

---
+++ Compete the skills - GFG/compete-the-skills.py
@@ -0,0 +1,53 @@
+#User function Template for python3
+
+class Solution:
+    def scores(self, a, b, cc):
+        # Update list cc of length 2 with cc[0] = ca and cc[1] = cb
+        # Your code goes here
+        ca =0
+        cb =0
+        for i in range(len(a)):
+            
+            if a[i] > b[i]:
+                ca +=1
+            elif a[i] < b[i]:
+                cb += 1
+            else:
+                continue
+        cc[0] = ca
+        cc[1] = cb
+        
+        
+    	
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+    	a = [int(x) for x in input().strip().split()]
+    	b = [int(x) for x in input().strip().split()]
+    	
+    	cc = [0, 0]
+    	ob=Solution()
+    	ob.scores(a, b, cc)
+    	print(*cc)
+    	
+    	T -= 1
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+# } Driver Code Ends
