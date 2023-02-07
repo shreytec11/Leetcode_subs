9abf9150dfa324afe4d8e1c245b52c2f97771b41
shreytec11 <71381075+shreytec11@users.noreply.github.com>
2023-02-07 15:50:09 +0530

    Added solution - LeetHub

---
+++ Game with nos - GFG/game-with-nos.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+def game_with_number (arr,  n) : 
+    #Complete the function
+    
+    for i in range(len(arr)):
+        
+        d = arr[i]
+        
+        if i == n-1:
+            arr[i] = d
+            break
+            
+        arr[i] = arr[i] ^ arr[i+1]
+    return arr
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    res = game_with_number(arr, n);
+    print(*res)
+
+
+
+
+# } Driver Code Ends
